0a782775e86e5058158897281722945cdbb3221c
Olivier VALLOD <olivier.vallod@fr.ibm.com>
2018-05-23 19:04:27 +0200

    Update create_all.sh

--- cs-offerings/scripts/create_all.sh
+++ cs-offerings/scripts/create_all.sh
@@ -29,7 +29,7 @@ CHANNEL_NAME="channel1" PEER_MSPID="Org2MSP" PEER_ADDRESS="blockchain-org2peer1:
 
 echo ""
 echo "=> CREATE_ALL: Creating composer playground"
-create/create_composer-playground.sh $@
+CHANNEL_NAME="channel1" create/create_composer-playground.sh $@
 
 # Can't create this until the user has performed manual actions in the Composer Playground.
 # echo ""
